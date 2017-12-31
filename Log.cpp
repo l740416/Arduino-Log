edf6a699d637a9932a8cf226e46557cb3b4eecb3
l740416 <j.huang.1985@gmail.com>
2017-12-31 15:55:37 -0800

    Support F() macro

--- Log.cpp
+++ Log.cpp
@@ -1,22 +1,25 @@
 
 #include <TimeLib.h> 
 #include "Arduino.h"
+#include <stdarg.h>
 #include "Log.h"
 
 Log::Log() {
-  memset( buf, 0, sizeof(buf) );
+  memset( buf, '\0', sizeof(buf) );
   idx = 0;
-  for( int i = 0; i < N_LOG_LOOP; i++ ) {
-    timestamp_enter[i] = 0;
-    timestamp_duration[i] = 0;
-  }
 }
 
 
-size_t Log::write(uint8_t v) {
-  memcpy( buf+idx, &v, 1 );
+size_t Log::write(uint8_t v)
+{
+	buf[idx] = v;
   idx += 1;
-  if( idx >= LOG_SIZE ) idx = 0;
+  buf[idx] = '\0';
+  if( idx >= LOG_SIZE )
+  {
+  	idx = 0;
+  	buf[0] = '\0';
+  }
   Serial.write( v );
   return 1;  
 }
@@ -29,39 +32,21 @@ size_t Log::write(const uint8_t *buffer, size_t size) {
 
   if( size >= 5 && buffer[0] == '[' && buffer[1] == 'M' && buffer[2] == 'S' && buffer[3] == 'G' && buffer[4] == ']' ) {
     char str[100];
-    snprintf( str, 100, "[%02d:%02d:%02d]", hour(), minute(), second() );
+    snprintf( str, 100, "[%02d%02d]", minute(), second() );
     write( str );
     write( buffer + 5, size - 5 );
     return size - 5 + strlen(str);
   } 
-
-  if( size >= LOG_SIZE ) return 0;
-  if( idx + size >= LOG_SIZE ) {
-    buf[idx] = '\0';
-    idx = 0;
-  }
-  memcpy( buf + idx, buffer, size );
-  idx += size;  
-
-  Serial.write( buffer, size );
-
   
-  /*
-  if( idx + size >= LOG_SIZE ) {
-    size_t n = LOG_SIZE - idx;
-    memcpy( buf + idx, buffer, n );
-    idx = size - n;
-    memcpy( buf, buffer + n, idx );
-    
-  } else {
-    memcpy( buf+idx, buffer, size );
-    idx += size;
+  for(size_t i = 0; i < size; i++)
+  {
+  	write(buffer[i]);
   }
-  */
+
   return size;  
 }
 
-size_t Log::printf( char *fmt, ... ) {
+size_t Log::printf( char const *fmt, ... ) {
   // resulting string limited to 256 chars
   char buf[256]; 
   va_list args;
@@ -71,12 +56,14 @@ size_t Log::printf( char *fmt, ... ) {
   return write( ( const uint8_t * ) buf, strlen(buf) );
 }
 
-void Log::show_status() {
-  printf( "[MSG]  Serial loop: %dus\n", duration(LOG_SERIAL_LOOP) );
-  printf( "[MSG]  Sensor loop: %dus\n", duration(LOG_SENSOR_LOOP) );
-  printf( "[MSG] Nodenet loop: %dus\n", duration(LOG_NODENET_LOOP) );
-  printf( "[MSG]    Wifi loop: %dus\n", duration(LOG_WIFI_LOOP) );
-  printf( "[MSG]    Http loop: %dus\n", duration(LOG_HTTP_LOOP) );
-  printf( "[MSG]     IoT loop: %dus\n", duration(LOG_IOT_LOOP) );
+#ifdef F // check to see if F() macro is available
+size_t Log::printf(const __FlashStringHelper *fmt, ...)
+{
+  char buf[256]; // resulting string limited to 128 chars
+  va_list args;
+  va_start (args, fmt);
+  vsnprintf_P(buf, sizeof(buf), (const char *)fmt, args); // progmem for AVR
+  va_end(args);
+  return write( ( const uint8_t * ) buf, strlen(buf) );
 }
-
+#endif
--- Log.h
+++ Log.h
@@ -4,54 +4,42 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-#define LOG_SIZE 4096
+#define LOG_SIZE 1024
 
 class Log : public Print 
 {
 
   public:
 
-    enum LOG_LOOP_TYPE {
-      LOG_SERIAL_LOOP = 0,
-      LOG_SENSOR_LOOP,
-      LOG_NODENET_LOOP,
-      LOG_WIFI_LOOP,
-      LOG_HTTP_LOOP,
-      LOG_IOT_LOOP,
-      N_LOG_LOOP
-    };
-  
     Log();
   
     virtual size_t write( uint8_t v );
     virtual size_t write( const char *str );
     virtual size_t write( const uint8_t *buffer, size_t size );
-    size_t printf( char *fmt, ... );
+    size_t printf( char const *fmt, ... );
+
+#ifdef F // check to see if F() macro is available
+		size_t printf(const __FlashStringHelper *format, ...);
+#endif
 
     const char * get() { return buf; }
     const char * get_first() { return buf+idx+1; }
     const char * get_second() { return buf; }
 
-    void enter( enum LOG_LOOP_TYPE type ) {
-      timestamp_enter[ type ] = micros();
+    void enter( int id ) {
+      funcId = id;
     }
     
-    void leave( enum LOG_LOOP_TYPE type ) {
-      timestamp_duration[ type ] = micros() - timestamp_enter[ type ];
+    int current_id() {
+      return funcId;
     }   
-    
-    long duration( enum LOG_LOOP_TYPE type ) {
-      return timestamp_duration[ type ];
-    }
 
-    void show_status();
     
   private:
-    char buf[LOG_SIZE];
+    char buf[LOG_SIZE+1];
     size_t idx;
-    long timestamp_enter[ N_LOG_LOOP ];
-    long timestamp_duration[ N_LOG_LOOP ];
-  
+
+		int funcId;  
 };
 
 
